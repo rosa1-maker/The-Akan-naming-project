7740e2d24c000b850e7a61a59d9368dd6c6dd7fa
rosa1-maker <teresawanjiku2000@gmail.com>
2019-12-02 08:45:23 +0300

    added function femaleNames to the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
-var submit=function(){
     var maleNames=(
     "Kwasi", "KWado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
     );
     var femaleNames=(
         "Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"
     );
-}
+    function femaleNames(){
+var day= parseInt(documentgetElementById("year").value);
+var monthh=parseInt(documentgetElementById("Month").value);
+var day =parseInt(documentgetElementById("day").value);
+var d=new Date =d(year, month, date);
+var yourDay=d.getDay();
+
+if(youDay==0){
+    return document.getElementById("result").innerHTML +="Akosua";
+};
+
+
+    }

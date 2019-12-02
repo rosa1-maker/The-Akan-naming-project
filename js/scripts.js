23afa76bac3c45f5315954dde909927daa27a7c8
rosa1-maker <teresawanjiku2000@gmail.com>
2019-12-02 09:24:51 +0300

    created a function maleNames

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
     "Kwasi", "KWado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
     );
     var femaleNames=(
-        "Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"
+        "Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua" "Ama"
     );
     function femaleNames(){
 var day= parseInt(documentgetElementById("year").value);
@@ -11,9 +11,65 @@ var day =parseInt(documentgetElementById("day").value);
 var d=new Date =d(year, month, date);
 var yourDay=d.getDay();
 
-if(youDay==0){
+if(yourDay==0){
     return document.getElementById("result").innerHTML +="Akosua";
+};
+if(yourDay==1){
+    return document.getElementById("result").innerHTML +="Adwoa";
+};
+
+if(yourDay==2){
+    return document.getElementById("result").innerHTML +="Abenaa";
+};
+
+if(yourDay==3){
+    return document.getElementById("result").innerHTML +="Akua";
+};
+
+if(yourDay==4){
+    return document.getElementById("result").innerHTML +="Yaa";
+};
+
+if(yourDay==5){
+    return document.getElementById("result").innerHTML +="Afua";
+};
+if(yourDay==6){
+    return document.getElementById("result").innerHTML +="Ama";
 };
 
 
     }
+    function maleNames(){
+        var day= parseInt(documentgetElementById("year").value);
+        var monthh=parseInt(documentgetElementById("Month").value);
+        var day =parseInt(documentgetElementById("day").value);
+        var d=new Date =d(year, month, date);
+        var yourDay=d.getDay();
+        
+        if(yourDay==0){
+            return document.getElementById("result").innerHTML +="Kwasi";
+        };
+        if(yourDay==1){
+            return document.getElementById("result").innerHTML +="Kwado";
+        };
+        
+        if(yourDay==2){
+            return document.getElementById("result").innerHTML +="Kwabena";
+        };
+        
+        if(yourDay==3){
+            return document.getElementById("result").innerHTML +="Kwaku";
+        };
+        
+        if(yourDay==4){
+            return document.getElementById("result").innerHTML +="Yaw";
+        };
+        
+        if(yourDay==5){
+            return document.getElementById("result").innerHTML +="Kofi";
+        };
+        if(yourDay==6){
+            return document.getElementById("result").innerHTML +="Kwame";
+        };
+                
+            }
